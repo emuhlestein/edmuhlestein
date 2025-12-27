023298ea527a8d43b89a236c45b4f5aef3242a78
emuhlestein <emuhlestein@yahoo.com>
2025-12-27 07:59:57 -0700

    add nav bar

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routers import (
     root_router,
     auth_router,
@@ -8,6 +9,8 @@ app = FastAPI(debug=True)
 
 app.include_router(root_router)
 app.include_router(auth_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # @app.get("/health")
 # def health():
