077bd0001e7650c1adfb287ba436b9fb671603b7
emuhlestein <emuhlestein@yahoo.com>
2025-12-27 08:36:58 -0700

    Fix css loading

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from routers import (
     root_router,
     auth_router,
 )
 
 app = FastAPI(debug=True)
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 app.include_router(root_router)
 app.include_router(auth_router)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 # @app.get("/health")
 # def health():
