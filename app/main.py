b0a78bbb2f8c31bcc752b01a1de8a3e32328dd6c
emuhlestein <emuhlestein@yahoo.com>
2026-09-21 20:15:18 -0600

    Add about page

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ from fastapi.staticfiles import StaticFiles
 
 from .database import engine  # sync engine (sqlalchemy.create_engine)
 from .models.base import Base   # DeclarativeBase subclass
-from .routers import root_router, auth_router
+from .routers import root_router, auth_router, about_router
 
 app = FastAPI(debug=True)
 app.mount("/static", StaticFiles(directory="/app/app/static", html=True), name="static")
 
 app.include_router(root_router)
 app.include_router(auth_router)
+app.include_router(about_router)
 
 @app.on_event("startup")
 def on_startup():
