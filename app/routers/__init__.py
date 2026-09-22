dde637bc1ce86fd6150eae38ba787d963cdffefd
Ed Muhlestein <emuhlestein@yahoo.com>
2026-09-21 20:18:56 -0600

    Add about page (#17)

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,8 +1,10 @@
 
 from .root import router as root_router
 from .auth import router as auth_router
+from .about import router as about_router
 
 __all__ = [
     "root_router",
     "auth_router",
+    "about_router",
 ]
